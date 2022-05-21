611d14363e43b42828ab69ce247794d58de69785
Alline Oliveira allineo <alline.oliveira@gmail.com>
2022-05-21 19:39:36 -0300

    'alias'

---
+++ admin.js
@@ -0,0 +1,61 @@
+var http = require("http");
+var axios = require('axios');
+
+http.createServer(function (request, response) {
+    const head = {
+        'Content-Type': 'text/plain',
+      //  'charset': 'utf-8'
+    };
+    response.writeHead(200, head);
+
+    let alias = getToken('65904249187');
+
+    response.end('Olá Mundo\n' + alias);
+}).listen(8000, '127.0.0.1');
+
+console.log('Servidor executando em http://127.0.0.1:8000/');
+
+  
+// Collection Postman: https://www.getpostman.com/collections/fe186c20177c57fb8041
+// https://www.getpostman.com/collections/8642d8b8301dd4bbdd2d
+
+let liveonCredentials = {
+    "url": "https://lotus-prod-apim.baas.solutions/crebit",
+    "subscriptionKey": "16d9f23318a14fe38555008356a59854",
+    "Authorization": "MFY2MzlPNVF5b0lpNDNINGh0RFhIQWVUNDMrUzh2OGxNdDY1bGJ4RjVzUT06U1Z0YmZ6QmxZV1Y3dkU0WTRDRU5jaE16OXFYOE11M3Z6enpNWmxUNzNZclI0b0NrM0VsdzltM0h2Nm12RmxYVllHRFVEWW5FeXY2UVBNTUJZSWY0V2c9PQ==",
+    "adminpwd": "70c071daac26ae7b"
+};
+
+async function getToken(cpf) {
+  try {
+      console.log(cpf);
+      const url = liveonCredentials['url'] + '/auth';
+      const headers = {
+          'Content-Type': 'application/json',
+          'Subscription-key': liveonCredentials['subscriptionKey']
+      }
+      const access = cpf.substring(3, 9).split("").reverse().join("");
+      const data = JSON.stringify({
+          "document": cpf,
+          "password": access
+          //"is_android": true
+      });
+      const resp = await axios.post(url, data, {
+          headers: headers
+      })
+          .then(function (response) {
+              console.log(response.data);
+              return response.data;
+          })
+          .catch(function (error) {
+              console.log('error.response.data: ' + JSON.stringify(error.response.data));
+              console.log('error.config: ' + JSON.stringify(error.config));
+              //console.log(error);
+          });
+
+        return resp;
+      //return resp['token'];
+  } catch (_error) {
+      console.log("getToken " + _error);
+  }
+}
